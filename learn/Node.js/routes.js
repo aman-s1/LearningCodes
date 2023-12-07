ec3ead6d2013d170bb72525ff5ddf60b4d5e4d64
aman-s1 <amansuhag6014@gmail.com>
2023-12-07 12:17:16 +0530

    nodemon

--- learn/Node.js/routes.js
+++ learn/Node.js/routes.js
@@ -50,4 +50,15 @@ const requestHandler = (req,res) => {
   }
 }
 
-module.exports = requestHandler;
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
