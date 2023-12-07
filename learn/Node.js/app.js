2e1d10ea3520398cae4f06290c8f2f724a04883f
aman-s1 <amansuhag6014@gmail.com>
2023-12-07 08:55:01 +0530

    newnew

--- learn/Node.js/app.js
+++ learn/Node.js/app.js
@@ -1,18 +1,39 @@
 const http = require('http');
-
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Log your name to the console
-  console.log('My name is Aman');
-
-  // Send a response to the browser
-  res.end('Hello from the server! Check the console for my name.');
+const fs = require('fs');
+const server = http.createServer((req,res) => {
+  const url = req.url;
+  const method = req.method;
+  if(url === '/'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt',message,err => {
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+      });
+    });
+  }
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  const mes = fs.readFile('message.txt',(data) => {
+    res.write('<body><h4>data</h4></body>');
+  });
+  res.write('</html>');
+  res.end();
 });
-
-// Listen on port 4000
-const PORT = 4000;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+server.listen(3000);
