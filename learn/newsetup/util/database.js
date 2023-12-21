ec129877194851480f7cc28a99352595191a33a9
aman-s1 <amansuhag6014@gmail.com>
2023-12-21 11:43:56 +0530

    sequelize

--- learn/newsetup/util/database.js
+++ learn/newsetup/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'aman'
+const sequelize = new Sequelize('node-complete','root','aman', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
