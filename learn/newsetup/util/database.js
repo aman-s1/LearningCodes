df8c52ced2b9cc03487b39b8a7c3d8b6655b428b
aman-s1 <amansuhag6014@gmail.com>
2023-12-21 19:59:54 +0530

    new

--- learn/newsetup/util/database.js
+++ learn/newsetup/util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete','root','aman', {
-    dialect: 'mysql',
-    host: 'localhost'
+const sequelize = new Sequelize('node-complete', 'root', 'aman', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
